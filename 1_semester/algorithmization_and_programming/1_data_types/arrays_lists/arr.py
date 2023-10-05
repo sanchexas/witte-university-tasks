e375e6f3c0699b8ef3c606bbceddbc142dd59626
sergey-sanches-peres <sanchesgithub@mail.ru>
2023-10-05 21:12:45 +0300

    arrs and files

---
+++ 1_semester/algorithmization_and_programming/1_data_types/arrays_lists/arr.py
@@ -0,0 +1,14 @@
+arr = [1, "hello", 23, [1,2,3]]
+
+print(len(arr)) # длина массива
+arr.append(1) # добавление элемента в конец
+print(arr)
+arr.remove(1) # удаляет только первый попавшийся элемент
+print(arr)
+numsArr = [2,1,4,8,5]
+numsArr.sort() # сортирует массив чисел
+print(numsArr)
+numsArr.reverse() # сортирует по убыванию
+print(numsArr)
+arr.insert(0, "element") # вставляет элемент на определенную позицию
+print(arr)
