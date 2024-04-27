9bb801b519869a3f5f23c55420b768861990ec2d
sergey-sanches-peres <sanchesgithub@mail.ru>
2024-04-28 01:00:14 +0300

    done with rating work 2

---
+++ 2_semester/rating_works/python-rating-work/main.py
@@ -0,0 +1,39 @@
+# Работа выполнена студентом 1-го курса группы "о.ИЗДтс 23.1/Б3-23" университета МУИВ
+# Санчес-Перес Сергеем Евгеньевичем
+# 😸 Мой GitHub 😸 - https://github.com/sanchexas
+
+# -Кастомные функции
+from my_functions import *
+from typing import List
+
+# -Получение текста из файла
+sourceFileData: str = readDataFromFile('source_data.txt')
+# -Разбитие строки на массив [ФИО, ID]
+fioAndIdList: list = customSplit(sourceFileData, '\n')
+# -Присваивание переменной ФИО
+fio: str = fioAndIdList[0]
+# -Присваивание переменной ID
+id: int = int(fioAndIdList[1])
+# -Удаление пробелов в ФИО
+removedSpacesFromFio: str = removeElementFromString(fio, ' ')
+# -Деление ID на длину ФИО (без учета пробелов)
+devideIdByFioResult: int = getIntegerByDevide(id, len(removedSpacesFromFio))
+# -Формирование набора данных из кодов Юникода
+dataList: List[int] = getUnicodeDataArray(removedSpacesFromFio)
+# -Копирование несортированного набора данных для будущего использования для записи в файл результата
+unSortedDataList: List[int] = dataList[:]
+# -Проверка на то, является ли результат деления ID на длину ФИО четным
+isDevideResultEven: bool = isEven(devideIdByFioResult)
+# -Алгоритм "быстрой сортировки" (Хоара)
+quickSortResult: List[int] = quickSort(dataList, isDevideResultEven)
+# -Алгоритм сортировки по методу "пузырька"
+bubbleSortResult: List[int] = bubbleSort(dataList, isDevideResultEven)
+# -Вычисление среднего арифметического значения набора данных
+arithmeticMeanResult: float = arithmeticMeanValue(quickSortResult)
+# -Вычисление среднего квадратического значения набора данных
+quadraticResult: float = quadraticValue(bubbleSortResult)
+# -Порядок сортировки
+orderMessage: float = 'по возрастанию' if isDevideResultEven else 'по убыванию'
+# -Запись в файл:
+# --функция toFixedValue - округляет значение до трех (по умолчанию) знаков после точки
+writeDataToFile('result.txt', f'1. Исходные данные: {fio}; ID: {id}\n2. {devideIdByFioResult}\n3. Направление сортировки {orderMessage}, так как число {devideIdByFioResult} - {"четное" if isDevideResultEven else "нечетное"}\n4. Набор данных: {unSortedDataList}\n5. Отсортированный {orderMessage} набор данных {quickSortResult}\n6. Среднее арифметическое значение: {toFixedValue(arithmeticMeanResult)}\n7. Среднее квадратическое значение: {toFixedValue(quadraticResult, 3) }')
--- 2_semester/rating_works/python-rating-work/my_functions.py
+++ 2_semester/rating_works/python-rating-work/my_functions.py
@@ -1,3 +1,7 @@
+# Работа выполнена студентом 1-го курса группы "о.ИЗДтс 23.1/Б3-23" университета МУИВ
+# Санчес-Перес Сергеем Евгеньевичем
+# 😸 Мой GitHub 😸 - https://github.com/sanchexas
+
 import math
 import random
 from typing import Final
@@ -5,13 +9,16 @@ from typing import List
 
 UTF_8: Final[str] = "utf-8"
 
+# -Чтение текста из файла
 def readDataFromFile(srcFile: str) -> str:
     data = open(file=srcFile, mode="r", encoding=UTF_8)
     return data.read()
 
+# -Добавление текста в файл
 def writeDataToFile(srcFile: str, data: str) -> None:
     open(file=srcFile, mode="w", encoding=UTF_8).write(data)
 
+# -Удалить элемент из строки
 def removeElementFromString(strOrigin: str, strElemToRemove: str) -> str:
     res: str = ""
     for s in strOrigin: 
@@ -19,6 +26,7 @@ def removeElementFromString(strOrigin: str, strElemToRemove: str) -> str:
             res += s
     return res
 
+# -Кастомный split()
 def customSplit(strOrigin: str, delimiters: str = ' \t\n'):
     res: list = []
     word: str = ""
@@ -32,14 +40,19 @@ def customSplit(strOrigin: str, delimiters: str = ' \t\n'):
         res.append(word)
     return res
 
+# -Деление
 def getIntegerByDevide(n1: int, n2: int) -> int:
     return int(n1/n2)
 
+# -Проверка числа на четность
 def isEven(n: int) -> bool:
     if(n % 2 == 0): return True 
     else: return False
 
-# order (порядок) : true - по возрастанию, false - по убыванию
+# -Алгоритм "быстрой сортировки" (Хоара)
+# --order (порядок) : true - по возрастанию, false - по убыванию
+# TODO Как-то можно оптимизировать данную функцию в плане условия с порядком (if order), но мне лень...
+
 def quickSort(numsArray: List[int], order: bool) -> List[int]:
     if len(numsArray) <= 1:
         return numsArray
@@ -66,7 +79,10 @@ def quickSort(numsArray: List[int], order: bool) -> List[int]:
                     medium.append(elem)
         return quickSort(left, order) + medium + quickSort(right, order)
 
-# order (порядок) : true - по возрастанию, false - по убыванию
+# -Алгоритм сортировки по методу "пузырька"
+# --order (порядок) : true - по возрастанию, false - по убыванию
+# TODO Как-то можно оптимизировать данную функцию в плане условия с порядком (if order), но мне лень...
+
 def bubbleSort(numsArray: List[int], order: bool) -> List[int]:
     n = len(numsArray)
     if(order):
@@ -81,25 +97,28 @@ def bubbleSort(numsArray: List[int], order: bool) -> List[int]:
                     numsArray[j], numsArray[j + 1] = numsArray[j + 1], numsArray[j]
     return numsArray
 
-def getDataArrayFromUnicode(strOrigin: str) -> List[int]:
+# -Формирование набора данных из кодов Юникода
+def getUnicodeDataArray(strOrigin: str) -> List[int]:
     arr: list = list(removeElementFromString(strOrigin, " "))
-    print(arr)
     res: list = []
     for character in arr:
         res.append(ord(character))
     return res
 
+# -Вычисление среднего арифметического значения набора данных
 def arithmeticMeanValue(array: list) -> float:
     sum: int = 0
     for num in array:
         sum += num
     return sum/len(array)
 
+# -Вычисление среднего квадратического значения набора данных
 def quadraticValue(array: list) -> float:
     sum: int = 0
     for num in array:
         sum += num**2
     return math.sqrt(sum/len(array))
 
-def toFixedValue(numObj, digitsAfterDot=3):
-    return f"{numObj:.{digitsAfterDot}f}"
+# -Округление значения до трех (по умолчанию) знаков после точки
+def toFixedValue(numObj, digitsAfterDot: int = 3) -> float:
+    return float(f"{numObj:.{digitsAfterDot}f}")
